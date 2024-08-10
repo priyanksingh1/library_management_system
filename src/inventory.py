52a2c6403e4af25b9446d4ce9c9ee59e4c38413b
priyanksingh <priyanksingh802@gmail.com>
2024-08-11 01:39:42 +0530

    method to view inventory

--- src/inventory.py
+++ src/inventory.py
@@ -2,3 +2,13 @@ class InventoryManager:
     def __init__(self):
         self.low_stock_threshold = 2
 
+    def view_inventory(self):
+        inventory = {}
+        for book in self.book_manager.books.values():
+            inventory[book.isbn] = {
+                "title": book.title,
+                "author": book.author,
+                "copies": book.copies,
+            }
+        return inventory
+
