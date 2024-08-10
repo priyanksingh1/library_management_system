1918819cd3009e368f83e490d125bd6830edc8df
priyanksingh <priyanksingh802@gmail.com>
2024-08-11 01:18:10 +0530

    test for checking over due books

--- tests/test_overdue.py
+++ tests/test_overdue.py
@@ -15,3 +15,11 @@ class TestOverdueManager(unittest.TestCase):
         self.user = self.user_manager.register_user("abc", "abc@example.com", "password123")
         self.book = self.book_manager.add_book("12345", "Python 101", "abc", 2023)
         self.borrowed_book = self.borrowing_manager.borrow_book(self.user.email, self.book.isbn)
+
+    def test_check_overdue_books(self):
+        # Simulate overdue by setting borrowed date in the past
+        self.borrowed_book.borrowed_date = date.today() - timedelta(days=15)
+        overdue_books = self.overdue_manager.check_overdue_books()
+        self.assertEqual(len(overdue_books), 1)
+        self.assertEqual(overdue_books[0].user.email, self.user.email)
+
