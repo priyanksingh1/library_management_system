c4645a33cc7b1caca7d9d765ef162414a451e8d4
priyanksingh <priyanksingh802@gmail.com>
2024-08-10 23:02:10 +0530

    test of adding book method

--- tests/test_book.py
+++ tests/test_book.py
@@ -6,4 +6,8 @@ class TestBookManager(unittest.TestCase):
     def setUp(self):
         self.book_manager = BookManager()
 
-#
+# method tests if a book can be added successfully.
+def test_add_book(self):
+    book = self.book_manager.add_book("12345", "Python 101", "John Doe", 2023)
+    self.assertIsInstance(book, Book)
+    self.assertEqual(book.title, "Python 101")
