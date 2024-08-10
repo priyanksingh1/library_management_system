dcbb9e2898329e548a2223db7ecb96cdbdf08997
priyanksingh <priyanksingh802@gmail.com>
2024-08-10 23:08:59 +0530

    test of edit book

--- tests/test_book.py
+++ tests/test_book.py
@@ -17,3 +17,14 @@ def test_add_duplicate_book(self):
     self.book_manager.add_book("12345", "Python 101", "abc", 2023)
     with self.assertRaises(ValueError):
         self.book_manager.add_book("12345", "Python 101", "abc", 2023)
+
+# This method verifies that an existing book can be edited. It checks that the title is updated correctly.
+def test_edit_book(self):
+    self.book_manager.add_book("12345", "Python 101", "abc", 2023)
+    updated_book = self.book_manager.edit_book("12345", title="Advanced Python")
+    self.assertEqual(updated_book.title, "Advanced Python")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
