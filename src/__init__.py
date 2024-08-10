57b5ec797ce73bd3d05b93ce62725c3f88a247e0
priyanksingh <priyanksingh802@gmail.com>
2024-08-11 01:08:40 +0530

    add method borrow book

--- src/__init__.py
+++ src/__init__.py
@@ -6,3 +6,15 @@ class BorrowingManager:
         self.borrowed_books = []
         self.renewal_period = timedelta(days=7)
 
+    def borrow_book(self, user_email, book_isbn):
+        # Check if the book is available
+        book = self._find_book(book_isbn)
+        if not book.available:
+            raise ValueError("Book is already borrowed or reserved.")
+
+        user = self._find_user(user_email)
+        borrowed_book = BorrowedBook(user, book)
+        self.borrowed_books.append(borrowed_book)
+        book.available = False
+        return borrowed_book
+
