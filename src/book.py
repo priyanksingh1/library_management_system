b902ae6babc904925dae1b3ea0218152574b5ff8
priyanksingh <priyanksingh802@gmail.com>
2024-08-10 23:28:29 +0530

    method of adding book

--- src/book.py
+++ src/book.py
@@ -4,3 +4,9 @@ class BookManager:
     def __init__(self):
         self.books = {}
 
+    def add_book(self, isbn, title, author, published_year):
+        if isbn in self.books:
+            raise ValueError("Book with this ISBN already exists.")
+        book = Book(isbn, title, author, published_year)
+        self.books[isbn] = book
+        return book
