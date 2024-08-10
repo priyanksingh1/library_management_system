283244a610ae0585e799d7cf8af25042f6a69dc1
priyanksingh <priyanksingh802@gmail.com>
2024-08-10 23:05:00 +0530

    test of adding duplicate book

--- tests/test_book.py
+++ tests/test_book.py
@@ -8,6 +8,12 @@ class TestBookManager(unittest.TestCase):
 
 # method tests if a book can be added successfully.
 def test_add_book(self):
-    book = self.book_manager.add_book("12345", "Python 101", "John Doe", 2023)
+    book = self.book_manager.add_book("12345", "Python 101", "abc", 2023)
     self.assertIsInstance(book, Book)
     self.assertEqual(book.title, "Python 101")
+
+# method tests the handling of duplicates. It adds a book and then tries to add the same book again, expecting a ValueError to be raised.
+def test_add_duplicate_book(self):
+    self.book_manager.add_book("12345", "Python 101", "abc", 2023)
+    with self.assertRaises(ValueError):
+        self.book_manager.add_book("12345", "Python 101", "abc", 2023)
